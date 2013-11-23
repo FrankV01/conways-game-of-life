54d6b87737a58222875cdffea2b506c314487db6
Frank Villasenor <frank.villasenor@gmail.com>
2013-11-22 20:31:49 -0600

    Grid renders out. This is good enough to move on.

--- Output/UserControls/Grid.cs
+++ Output/UserControls/Grid.cs
@@ -18,28 +18,38 @@ namespace Output.UserControls
         {
             InitializeComponent();
             _gridPixels = new List<Pixel>();
+
+            this.SuspendLayout();
             this.initGrid();
+            this.ResumeLayout();
         }
 
         private void initGrid()
         {
             //Lets start with a 50x50 grid. 
             Pixel _px = null;
-            for( int x = 0; x < 36; x++ )
+            FlowLayoutPanel _p = null;
+            for( int vertical_row = 0; vertical_row < 40; vertical_row++ )
             {
                 //each row, establish a flow panel. 
-                FlowLayoutPanel _p = new FlowLayoutPanel();
+                _p = new FlowLayoutPanel();
                 _p.BackColor = Color.DarkGray;
                 //_p.Location = new Point(1*x, 0);
                 _p.Width = this.Width;
                 _p.Height = 12;
                 _p.Margin = new Padding(0);
-                for (int y = 0; y < 50; y++)
+
+                for (int horizontal_row = 0; horizontal_row < 50; horizontal_row++)
                 {
                     _px = new Pixel();
                     _px.State = false; //Turn off. 
 
-                    _px.Address = new Point(x, y);
+                    _px.Address = new Point(vertical_row, horizontal_row);
+
+                    _px.MouseHover += delegate(object sender, EventArgs e)
+                    {
+                        this.ParentForm.Text = "Pixel: " + (sender as Pixel).Address.ToString();
+                    };
 
                     _px.DoubleClick +=  delegate(object sender, EventArgs e)
                     {
@@ -48,9 +58,11 @@ namespace Output.UserControls
 
                     _p.Controls.Add(_px);
                     _gridPixels.Add(_px); //Not sure we need this.
+                    _px = null;
                 }
-                 
+                
                 this.flowLayoutPanel1.Controls.Add(_p);
+                _p = null;
             }
             
         }
