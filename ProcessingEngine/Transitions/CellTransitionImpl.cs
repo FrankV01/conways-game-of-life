979607f8223f9acf0b1ee5c587fe1f219454047c
Frank Villasenor <frank.villasenor@gmail.com>
2013-11-23 23:25:20 -0600

    Progress on the transition impl and a couple of unit tests.

--- ProcessingEngine/Transitions/CellTransitionImpl.cs
+++ ProcessingEngine/Transitions/CellTransitionImpl.cs
@@ -29,17 +29,52 @@ namespace FrankVillasenor.Life.ProcessingEngine.Transitions
         /// 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
         /// 
         /// We can also unit test this...
+        /// 
+        /// NOTE: We only turn off the cell in question. -- Not surrounding ones.
+        /// NOTE: Anything in a corner can die. It won't come alive. (Ignore for now, not exactly right)
         /// </summary>
         /// <param name="currentState"></param>
         /// <returns></returns>
         public bool[,] ApplyTransition(bool[,] currentState)
         {
-             bool[,] initState = currentState.Clone() as bool[,];
+            bool[,] initState = currentState.Clone() as bool[,];
             
             //First step is to iterate through and identify if a cell is dies due to under population.
-            
-            
+
+
+            for (int x1 = 0; x1 < currentState.GetLongLength(0); x1++)
+            {
+                for( int y1 = 0; y1 < currentState.GetLongLength(1); y1++)
+                {
+                    bool[,] subSet = this.subSelectAroundCell(x1, y1, currentState);
+                    if (this.FewerThanTwoLiveNeighbours(subSet))
+                    {
+                        currentState[x1, y1] = false;
+                    }
+
+                    //Next step, I think.
+                }
+            }
+
+
             return currentState;
+        }
+
+        private bool FewerThanTwoLiveNeighbours(bool[,] subSet)
+        {
+            bool firstAlive = false;
+            bool secondAlive = false;
+
+            for (int x1 = 0; x1 < subSet.GetLongLength(0); x1++)
+                for (int y1 = 0; y1 < subSet.GetLongLength(1); y1++)
+                    if (x1 != 1 && y1 != 1)
+                        if (subSet[x1, y1])
+                            if (firstAlive)
+                                secondAlive = true;
+                            else
+                                firstAlive = true;
+
+            return !(firstAlive && secondAlive); //both need to be alive.
         }
 
         /// <summary>
@@ -53,11 +88,19 @@ namespace FrankVillasenor.Life.ProcessingEngine.Transitions
         {
             bool[,] subset = new bool[3, 3];
 
-            for(int x1 = -1; x1 < x+1; x1++)
+            for(int x1 = -1; x1 < x; x1++)
             {
-                for(int y1 = -1; y1 < y+1; y1++)
+                for(int y1 = -1; y1 < y; y1++)
                 {
-                    subset[x1+1, y1+1] = grid[x1,y1];
+                    try
+                    {
+                        subset[x1 + 1, y1 + 1] = grid[x + x1, y + y1];
+                    }
+                    catch (IndexOutOfRangeException ex)
+                    {
+                        //If we are out of range, then the node doesn't exist and we should assume false in those regions.
+                        subset[x1 + 1, y1 + 1] = false;
+                    }
                 }
             }
 
---
+++ ProcessingEngineUnitTests/TransitionTests/BasicTransitionTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrankVillasenor.Life.ProcessingEngine.Transitions;
+
+namespace FrankVillasenor.Life.ProcessingEngineUnitTests.TransitionTests
+{
+    [TestClass]
+    public class BasicTransitionTest
+    {
+        [TestMethod]
+        public void BasicTransitionTest_SameSizeInVsOut()
+        {
+            bool[,] startingPoint = {
+                                   {false,false,false},
+                                   {false,true,false},
+                                   {false,false,false}
+                               };
+
+            int x = 3; int y = 3;
+
+            ICellTransition _trans = new CellTransitionImpl();
+            bool[,] result = _trans.ApplyTransition(startingPoint);
+
+
+
+            Assert.AreEqual(startingPoint.GetLongLength(0), result.GetLongLength(0));
+            Assert.AreEqual(startingPoint.GetLongLength(1), result.GetLongLength(1));
+
+            try
+            {
+                startingPoint.GetLongLength(2); //Should throw exception because we are not expected a new dim.
+                Assert.Fail("Unexpected LongLength");
+            }
+            catch (Exception ex) 
+            { 
+                //Passes test.
+            }
+        }
+
+
+        [TestMethod]
+        public void BasicTransitionTest_SingleCellDeath()
+        {
+            bool[,] startingPoint = {
+                                   {false,false,false},
+                                   {false,true,false},
+                                   {false,false,false}
+                               };
+
+            bool[,] expected = {
+                                   {false,false,false},
+                                   {false,false,false},
+                                   {false,false,false}
+                               };
+
+
+            ICellTransition _trans = new CellTransitionImpl();
+            bool[,] result = _trans.ApplyTransition(startingPoint);
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    Assert.AreEqual(expected[i, j], result[i, j]);
+        }
+
+        [TestMethod]
+        public void BasicTransitionTest_SingleCellLives()
+        {
+            bool[,] startingPoint = {
+                                   {false,false,false},
+                                   {true,true,true},
+                                   {false,false,false}
+                               };
+
+            bool[,] expected = {
+                                   {false,true,false},
+                                   {false,true,false},
+                                   {false,true,false}
+                               };
+
+
+            ICellTransition _trans = new CellTransitionImpl();
+            bool[,] result = _trans.ApplyTransition(startingPoint);
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    Assert.AreEqual(expected[i, j], result[i, j]);
+        }
+
+        //Many more tests are needed here. This Testclass is testing the majority of the "important" logic.
+    }
+}
