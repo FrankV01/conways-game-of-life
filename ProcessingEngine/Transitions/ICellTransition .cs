4101017605b0e153c0cae461c12c0d59504b8236
Frank Villasenor <frank.villasenor@gmail.com>
2013-11-23 19:41:27 -0600

    Interface and notes regarding how we'll handle transitions.

---
+++ ProcessingEngine/Transitions/ICellTransition .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrankVillasenor.Life.ProcessingEngine.Transitions
+{
+    /// <summary>
+    /// Interface to define a CellTransition class. 
+    /// Classes that implement this interface transition a
+    /// a grid (the whole thing??) to it's new state. 
+    /// </summary>
+    public interface ICellTransition
+    {
+        bool[,] ApplyTransition(bool[,] currentState);
+    }
+}
