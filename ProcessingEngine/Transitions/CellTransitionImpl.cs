59a5f822a99a59d3165c6ca142f7bc3479cce424
Frank Villasenor <frank.villasenor@gmail.com>
2013-11-24 00:00:44 -0600

    Passes all but BasicTransitionTest_SingleCellLives

--- ProcessingEngine/Transitions/CellTransitionImpl.cs
+++ ProcessingEngine/Transitions/CellTransitionImpl.cs
@@ -38,7 +38,7 @@ namespace FrankVillasenor.Life.ProcessingEngine.Transitions
         public bool[,] ApplyTransition(bool[,] currentState)
         {
             bool[,] initState = currentState.Clone() as bool[,];
-            
+            bool[,] revisedState = currentState.Clone() as bool[,];
             //First step is to iterate through and identify if a cell is dies due to under population.
 
 
@@ -47,11 +47,39 @@ namespace FrankVillasenor.Life.ProcessingEngine.Transitions
                 for( int y1 = 0; y1 < currentState.GetLongLength(1); y1++)
                 {
                     bool[,] subSet = this.subSelectAroundCell(x1, y1, currentState);
-                    if (this.FewerThanTwoLiveNeighbours(subSet))
-                    {
-                        currentState[x1, y1] = false;
-                    }
 
+                    //bool fewerThanTwoLiveNeighbours = this.FewerThanTwoLiveNeighbours(subSet);
+                    //bool twoOrThreeNeighbours = this.TwoOrThreeNeighbours(subSet);
+                    //bool tooManyLiveNeighbours = this.TooManyLiveNeighbours(subSet);
+
+                    //bool exactlyThreeLiveNeighbours = this.ExactlyThreeLiveNeighbours(subSet);
+
+
+                    bool isAlive = currentState[x1, y1];
+
+                    if (isAlive)
+                        currentState[x1, y1] = this.IsAliveCheck(subSet);
+                    else
+                        currentState[x1, y1] = this.ExactlyThreeLiveNeighbours(subSet);
+
+                    
+
+                    //if (currentState[x1, y1] && this.FewerThanTwoLiveNeighbours(subSet))
+                    //{
+                    //    currentState[x1, y1] = false;
+                    //}
+                    //else if (currentState[x1, y1] && this.TwoOrThreeNeighbours(subSet))
+                    //{
+                    //    break; //It lives on.
+                    //}
+                    //else if (currentState[x1, y1] && this.TooManyLiveNeighbours(subSet))
+                    //{
+                    //    currentState[x1, y1] = false;
+                    //}
+                    //else if (!currentState[x1, y1] && this.ExactlyThreeLiveNeighbours(subSet))
+                    //{
+                    //    currentState[x1, y1] = true;
+                    //}
                     //Next step, I think.
                 }
             }
@@ -60,22 +88,102 @@ namespace FrankVillasenor.Life.ProcessingEngine.Transitions
             return currentState;
         }
 
-        private bool FewerThanTwoLiveNeighbours(bool[,] subSet)
+        private bool ExactlyThreeLiveNeighbours(bool[,] subSet)
         {
-            bool firstAlive = false;
-            bool secondAlive = false;
+            int NumNeighbours = 0;
 
             for (int x1 = 0; x1 < subSet.GetLongLength(0); x1++)
                 for (int y1 = 0; y1 < subSet.GetLongLength(1); y1++)
-                    if (x1 != 1 && y1 != 1)
+                    if (x1 != 1 && y1 != 1) //Origin (the cell in question)
                         if (subSet[x1, y1])
-                            if (firstAlive)
-                                secondAlive = true;
-                            else
-                                firstAlive = true;
+                            NumNeighbours++;
 
-            return !(firstAlive && secondAlive); //both need to be alive.
+            return (NumNeighbours == 3);
         }
+
+        /// <summary>
+        /// 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        /// 2. Any live cell with two or three live neighbours lives on to the next generation.
+        /// 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+        /// </summary>
+        /// <param name="subSet"></param>
+        /// <returns> true if should stay alive, false otherwise</returns>
+        private bool IsAliveCheck(bool[,] subSet)
+        {
+            int NumNeighbours = 0;
+
+            for (int x1 = 0; x1 < subSet.GetLongLength(0); x1++)
+                for (int y1 = 0; y1 < subSet.GetLongLength(1); y1++)
+                    if (x1 != 1 && y1 != 1) //Origin (the cell in question)
+                        if (subSet[x1, y1])
+                            NumNeighbours++;
+
+            if (NumNeighbours > 2 || NumNeighbours < 3)
+                return false;
+            else //if (NumNeighbours == 2 || NumNeighbours == 3)
+                return true;
+        }
+
+        ///// <summary>
+        ///// 
+        ///// Any live cell with more than three live neighbours dies, as if by overcrowding.
+        ///// 
+        ///// NOTE: This and TwoOrThreeNeighbours could easily be combined.
+        ///// </summary>
+        ///// <param name="subSet"></param>
+        ///// <returns></returns>
+        //private bool TooManyLiveNeighbours(bool[,] subSet)
+        //{
+        //    int NumNeighbours = 0;
+
+        //    for (int x1 = 0; x1 < subSet.GetLongLength(0); x1++)
+        //        for (int y1 = 0; y1 < subSet.GetLongLength(1); y1++)
+        //            if (x1 != 1 && y1 != 1) //Origin (the cell in question)
+        //                if (subSet[x1, y1])
+        //                    NumNeighbours++;
+
+        //    return (NumNeighbours > 2);
+        //}
+
+        ///// <summary>
+        ///// Any live cell with two or three live neighbours lives on to the next generation.
+        ///// </summary>
+        ///// <param name="subSet"></param>
+        ///// <returns></returns>
+        //private bool TwoOrThreeNeighbours(bool[,] subSet)
+        //{
+        //    int NumNeighbours = 0;
+
+        //    for (int x1 = 0; x1 < subSet.GetLongLength(0); x1++)
+        //        for (int y1 = 0; y1 < subSet.GetLongLength(1); y1++)
+        //            if (x1 != 1 && y1 != 1) //Origin (the cell in question)
+        //                if (subSet[x1, y1])
+        //                    NumNeighbours++;
+
+        //    return (NumNeighbours == 2 || NumNeighbours == 3);
+        //}
+
+        ///// <summary>
+        ///// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        ///// </summary>
+        ///// <param name="subSet"></param>
+        ///// <returns></returns>
+        //private bool FewerThanTwoLiveNeighbours(bool[,] subSet)
+        //{
+        //    bool firstAlive = false;
+        //    bool secondAlive = false;
+
+        //    for (int x1 = 0; x1 < subSet.GetLongLength(0); x1++)
+        //        for (int y1 = 0; y1 < subSet.GetLongLength(1); y1++)
+        //            if (x1 != 1 && y1 != 1) //Origin (the cell in question)
+        //                if (subSet[x1, y1])
+        //                    if (firstAlive)
+        //                        secondAlive = true;
+        //                    else
+        //                        firstAlive = true;
+
+        //    return !(firstAlive && secondAlive); //both need to be alive.
+        //}
 
         /// <summary>
         /// Given x,y sub-select elements around
