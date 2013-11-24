8b1bf1444e10b54a5b304421137ea38dc869fbc6
Frank Villasenor <frank.villasenor@gmail.com>
2013-11-24 13:14:38 -0600

    Minor UI Improvements.

--- Output/Main.Designer.cs
+++ Output/Main.Designer.cs
@@ -29,20 +29,23 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblGenNum = new System.Windows.Forms.Label();
+            this.lblGen = new System.Windows.Forms.Label();
             this.cbCellList = new System.Windows.Forms.ComboBox();
             this.btnStop = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.grid1 = new FrankVillasenor.Life.UI.UserControls.Grid();
-            this.lblGen = new System.Windows.Forms.Label();
-            this.lblGenNum = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblCellTemplates = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.lblGenNum);
-            this.groupBox1.Controls.Add(this.lblGen);
+            this.groupBox1.Controls.Add(this.lblCellTemplates);
+            this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.cbCellList);
             this.groupBox1.Controls.Add(this.btnStop);
             this.groupBox1.Controls.Add(this.btnStart);
@@ -51,13 +54,32 @@
             this.groupBox1.Size = new System.Drawing.Size(233, 615);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            // 
+            // lblGenNum
+            // 
+            this.lblGenNum.AutoSize = true;
+            this.lblGenNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenNum.Location = new System.Drawing.Point(85, 63);
+            this.lblGenNum.Name = "lblGenNum";
+            this.lblGenNum.Size = new System.Drawing.Size(51, 55);
+            this.lblGenNum.TabIndex = 4;
+            this.lblGenNum.Text = "0";
+            // 
+            // lblGen
+            // 
+            this.lblGen.AutoSize = true;
+            this.lblGen.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGen.Location = new System.Drawing.Point(23, 26);
+            this.lblGen.Name = "lblGen";
+            this.lblGen.Size = new System.Drawing.Size(175, 37);
+            this.lblGen.TabIndex = 3;
+            this.lblGen.Text = "Generation";
             // 
             // cbCellList
             // 
             this.cbCellList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbCellList.FormattingEnabled = true;
-            this.cbCellList.Location = new System.Drawing.Point(6, 19);
+            this.cbCellList.Location = new System.Drawing.Point(6, 32);
             this.cbCellList.Name = "cbCellList";
             this.cbCellList.Size = new System.Drawing.Size(221, 21);
             this.cbCellList.TabIndex = 2;
@@ -65,7 +87,7 @@
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(6, 75);
+            this.btnStop.Location = new System.Drawing.Point(6, 88);
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(221, 23);
             this.btnStop.TabIndex = 1;
@@ -75,7 +97,7 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(6, 46);
+            this.btnStart.Location = new System.Drawing.Point(6, 59);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(221, 23);
             this.btnStart.TabIndex = 0;
@@ -100,25 +122,24 @@
             this.grid1.Size = new System.Drawing.Size(677, 616);
             this.grid1.TabIndex = 2;
             // 
-            // lblGen
+            // groupBox2
             // 
-            this.lblGen.AutoSize = true;
-            this.lblGen.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGen.Location = new System.Drawing.Point(30, 145);
-            this.lblGen.Name = "lblGen";
-            this.lblGen.Size = new System.Drawing.Size(175, 37);
-            this.lblGen.TabIndex = 3;
-            this.lblGen.Text = "Generation";
+            this.groupBox2.Controls.Add(this.lblGenNum);
+            this.groupBox2.Controls.Add(this.lblGen);
+            this.groupBox2.Location = new System.Drawing.Point(6, 134);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(221, 144);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
             // 
-            // lblGenNum
+            // lblCellTemplates
             // 
-            this.lblGenNum.AutoSize = true;
-            this.lblGenNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGenNum.Location = new System.Drawing.Point(89, 182);
-            this.lblGenNum.Name = "lblGenNum";
-            this.lblGenNum.Size = new System.Drawing.Size(51, 55);
-            this.lblGenNum.TabIndex = 4;
-            this.lblGenNum.Text = "0";
+            this.lblCellTemplates.AutoSize = true;
+            this.lblCellTemplates.Location = new System.Drawing.Point(3, 16);
+            this.lblCellTemplates.Name = "lblCellTemplates";
+            this.lblCellTemplates.Size = new System.Drawing.Size(76, 13);
+            this.lblCellTemplates.TabIndex = 6;
+            this.lblCellTemplates.Text = "Cell Templates";
             // 
             // Main
             // 
@@ -132,6 +153,8 @@
             this.Load += new System.EventHandler(this.OnLoad);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -146,6 +169,8 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label lblGenNum;
         private System.Windows.Forms.Label lblGen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblCellTemplates;
     }
 }
 
