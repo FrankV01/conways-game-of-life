da4861d3b1c99d2714a899dcb5b255fb7c4fe0b0
Frank Villasenor <frank.villasenor@gmail.com>
2013-11-23 20:54:00 -0600

    An inital attempt at processing the transitions.

---
+++ ProcessingEngine/Transitions/CellTransitionImpl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrankVillasenor.Life.ProcessingEngine.Transitions
+{
+    /// <summary>
+    /// This processes the transition logic. 
+    /// This is a first attempt at implementation. lets see
+    /// where we get.
+    /// </summary>
+    public class CellTransitionImpl : ICellTransition
+    {
+
+        public CellTransitionImpl()
+        {
+        }
+
+
+
+        /// <summary>
+        /// Rules. I believe the order applied does matter. (which should make multi-threading pretty easy.)
+        /// 
+        /// 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        /// 2. Any live cell with two or three live neighbours lives on to the next generation.
+        /// 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+        /// 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        /// 
+        /// We can also unit test this...
+        /// </summary>
+        /// <param name="currentState"></param>
+        /// <returns></returns>
+        public bool[,] ApplyTransition(bool[,] currentState)
+        {
+            bool[,] initState = currentState;
+
+
+            //If I modify currentState does it affect initState
+            currentState[0, 0] = true;
+            if (currentState[0, 0] != initState[0, 0])
+            {
+                System.Diagnostics.Debug.WriteLine("Worked");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Not expected");
+            }
+            return currentState;
+        }
+    }
+}
